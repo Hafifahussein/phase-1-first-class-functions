e8cc440e4d4f95938fb5b3530ebded3debab8785
Hafifa <hafifa.hussein@student.moringaschool.com>
2025-06-06 22:21:42 +0300

    commits

--- index.js
+++ index.js
@@ -13,3 +13,33 @@ function returnsAnAnonymousFunction() {
     console.log("I'm an anonymous function!");
   };
 }
+
+
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+};
+
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  };
+}
+
+
+const fareDoubler = createFareMultiplier(2);
+
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, selectionFunction) {
+  return selectionFunction(drivers);
+}
+
